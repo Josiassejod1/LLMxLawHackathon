cce61b37b6ef1ac1759090116ea772b6cd6e9717
Emanuel F. <70411130+efrancisworks@users.noreply.github.com>
2024-04-07 14:08:44 -0700

    added basic pdf view

--- tenant-chat/pages/pdf.js
+++ tenant-chat/pages/pdf.js
@@ -1,4 +1,8 @@
 import * as React from "react";
+import { Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer';
+// import { readFolderContents } from "../components/viewfiles";
+const document = '/TestData/California-Tenants-Guide.pdf';
+
 
 export default function Pdf() {
   return (
@@ -6,7 +10,17 @@ export default function Pdf() {
       <h1>About Us</h1>
       <p>
         We are a company that is dedicated to making the world a better place.
-      </p>
+      </p>      
+
+      <iframe
+     src={document}
+     width="100%"
+     height="600px"
+     loading="lazy"
+     title="PDF-file"></iframe>
+
     </div>
+
   );
 }
+
