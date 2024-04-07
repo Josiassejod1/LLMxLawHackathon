ab5c0fcf10d9af283defae2b5975630aa69d2fc6
Dalvin Josias Sejour <dalvin.josias@gmail.com>
2024-04-07 11:56:07 -0700

    reading is fundamental

--- tenant-chat/pages/chat.js
+++ tenant-chat/pages/chat.js
@@ -1,18 +1,31 @@
-import { ReactChatbot } from "@vectara/react-chatbot";
+"use client";
+import React, { useEffect, useState } from "react";
 
 export default function Chat() {
-  return (
-    <div>
-      <ReactChatbot
-        customerId={process.env.CUSTOMER_ID}
-        corpusId={[process.env.CORPUS_ID]}
-        apiKey={process.env.API_KEY}
-        title="My Chatbot"
-        placeholder="Chat with your AI assistant"
-        inputSize="large"
-        isInitiallyOpen={true}
-        enableStreaming={true}
-      />
-    </div>
-  );
+  const [chatWidget, setChatWidget] = useState(null);
+
+  /* the rest of your code */
+
+  useEffect(() => {
+    const importAndCreateWidget = async () => {
+      const { ReactChatbot } = await import("@vectara/react-chatbot");
+
+      setChatWidget(
+        <ReactChatbot
+          customerId={process.env.CUSTOMER_ID}
+          corpusId={[process.env.CORPUS_ID]}
+          apiKey={process.env.API_KEY}
+          title="My Chatbot"
+          placeholder="Chat with your AI assistant"
+          inputSize="large"
+          isInitiallyOpen={true}
+          enableStreaming={true}
+        />
+      );
+    };
+
+    importAndCreateWidget();
+  }, []);
+
+  return <div>{chatWidget}</div>;
 }
