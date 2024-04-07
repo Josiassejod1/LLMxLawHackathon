98f2ba3f592e5e58038eca6be8950bcc190ed4b9
Dalvin Josias Sejour <dalvin.josias@gmail.com>
2024-04-07 15:06:49 -0700

    fix

--- tenant-chat/pages/pdf.js
+++ tenant-chat/pages/pdf.js
@@ -1,8 +1,6 @@
 import * as React from "react";
-import { Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer';
 // import { readFolderContents } from "../components/viewfiles";
-const document = '/TestData/California-Tenants-Guide.pdf';
-
+const document = "/TestData/California-Tenants-Guide.pdf";
 
 export default function Pdf() {
   return (
@@ -10,17 +8,15 @@ export default function Pdf() {
       <h1>About Us</h1>
       <p>
         We are a company that is dedicated to making the world a better place.
-      </p>      
+      </p>
 
       <iframe
-     src={document}
-     width="100%"
-     height="600px"
-     loading="lazy"
-     title="PDF-file"></iframe>
-
+        src={document}
+        width="100%"
+        height="600px"
+        loading="lazy"
+        title="PDF-file"
+      ></iframe>
     </div>
-
   );
 }
-
