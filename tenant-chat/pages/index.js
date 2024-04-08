1500a44470d1255ce0003a880a9619f0856c35d9
Dalvin Josias Sejour <dalvin.josias@gmail.com>
2024-04-07 22:12:28 -0700

    tailwind

--- tenant-chat/pages/index.js
+++ tenant-chat/pages/index.js
@@ -1,22 +1,42 @@
 import Link from "next/link";
 
 import * as React from "react";
+const document = "/TestData/tenantHero.pdf";
 
 export default function Home() {
   return (
-    <ul>
-      <li>
-        <Link href="/">Home</Link>
-      </li>
-      <li>
-        <Link href="/pdf">About Us</Link>
-      </li>
-      <li>
-        <Link href="/chat">Chat</Link>
-      </li>
-      <li>
-        <Link href="/chatv2">Chatv2</Link>
-      </li>
-    </ul>
+    <div>
+      <nav>
+        <ul class="flex">
+          <li class="mr-6">
+            <Link href="/" className="relative px-4 py-2">
+              Home
+            </Link>
+          </li>
+          <li class="mr-6">
+            <Link href="/pdf" className="relative px-4 py-2">
+              Pdf
+            </Link>
+          </li>
+          <li class="mr-6">
+            <Link href="/chat" className="relative px-4 py-2">
+              Chat
+            </Link>
+          </li>
+          <li class="mr-6">
+            <Link href="/chatv2" className="relative px-4 py-2">
+              Chatv2
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <iframe
+        src={document}
+        width="100%"
+        height="600px"
+        loading="lazy"
+        title="PDF-file"
+      ></iframe>
+    </div>
   );
 }
